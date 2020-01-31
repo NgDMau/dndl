dc5125f38e94ef3b94d4939890a5e2e6c8c986ca
NgDMau <maunguyen.dinh@gmail.com>
2020-01-31 17:08:49 +0700

    fix pathname bug

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ const server = http.createServer((req, res) => {
         res.writeHead(200, {'Content-Type': 'text/html'});
         res.write("<h1>Hello, this is DNDL project<h1>");
         res.end();
+        return 
     }
     var filename = "." + q.pathname;
     fs.readFile(filename, function(err, data){
