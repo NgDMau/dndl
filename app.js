0c006ceb17fd57a461806ec30f68b39410e5f37f
NgDMau <maunguyen.dinh@gmail.com>
2020-02-07 15:27:58 +0700

    fix pathname in router

--- app.js
+++ app.js
@@ -1,26 +1,21 @@
 const express = require('express')
+const path = require('path')
+
 const app = express()
 const port = process.env.PORT || 8000;
 
 app.use(express.static('public'))
 
-var path = require('path')
-var fs = require('fs')
-
-function displayPage(filename, req, res) {
-    fs.readFile(filename, function (error, content) {
-        res.send(content)
-    })
-}
-
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, '/', 'index.html'))
+});
+
+app.get(filename, function (req, res) {
+    res.sendFile(path.join(__dirname, '/', filename))
 });
 
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, '/', 'about.html'))
 });
-
-
 
 app.listen(port, () => console.log(`Example app listening on port ${port}`))
