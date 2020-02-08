e699c87fd1312429e216b9723f2b87ab444898ef
NgDMau <maunguyen.dinh@gmail.com>
2020-02-08 15:47:14 +0700

    add experimental middleware

--- app.js
+++ app.js
@@ -4,18 +4,35 @@ const path = require('path')
 const app = express()
 const port = process.env.PORT || 8000;
 
-app.use(express.static('public'))
+var logger = function(req, res, next) {
+    console.log('Got Request!');
+    console.log(req.originalUrl);
+    next();
+}
+
+app.use(logger);
+
+app.use(express.static('public'));
+app.use(express.static('view'));
+
+app.set('view engine', 'pug')
 
 app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, '/', 'index.html'))
+    console.log(req.originalUrl);
+    res.sendFile(path.join(__dirname, '/views/', 'index.html'));
 });
 
 app.get('/index', function (req, res) {
-    res.sendFile(path.join(__dirname, '/', 'index.html'))
+    res.sendFile(path.join(__dirname, '/views/', 'index.html'))
 });
 
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, '/', 'about.html'))
+app.get('/about', function (req, res) {
+    console.log(req.originalUrl);
+    res.sendFile(path.join(__dirname, '/views/', 'about.html'))
+});
+
+app.get('/demo', function(req, res) {
+    res.render('demo');
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}`))
