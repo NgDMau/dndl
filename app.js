087bf1d2dc8acc79fca9afa446dc0480c071fbad
NgDMau <maunguyen.dinh@gmail.com>
2020-02-27 11:30:58 +0700

    add login authenticate

--- app.js
+++ app.js
@@ -1,21 +1,75 @@
 const express = require('express')
 const path = require('path')
+const db = require('./local_modules/database')
+
+var flash=require("connect-flash");
+var session = require('express-session');
+const bodyParser = require('body-parser');
+var passport = require('passport');
+var localStrategy = require('passport-local').Strategy;
 
 const app = express()
 const port = process.env.PORT || 8000;
 
-var logger = function(req, res, next) {
-    console.log('Got Request!');
-    console.log(req.originalUrl);
-    next();
-}
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(passport.initialize()); 
 
-app.use(logger);
+// var logger = function(req, res, next) {
+//     console.log('Got Request!');
+//     console.log(req.originalUrl);
+//     next();
+// }
+
+// app.use(logger);
+
+passport.use(new localStrategy(
+    function(username, password, done) {
+        // db.users.findByUsername(username, function(err, user) {
+        //     if (err) { return cb(err); }
+        //     if (!user) { return cb(null, false, { message: 'Incorrect username.' }) }
+        //     if (user.password != password) { 
+        //         return cb(null, false, { message: 'Incorrect password.' }); }
+        //     return cb(null, user);
+        if (username == 'mau') {
+            if (password == 'infore2811') {
+                return done(null, username)
+            } else {
+                return done(null, false);
+            }
+        } else {
+            return done(null, false);
+        }
+    })
+);
+
+passport.serializeUser((username, done) => {
+    done(null, username);
+})
+
+passport.deserializeUser((name, done) => {
+    //tại đây hứng dữ liệu để đối chiếu
+    if (name == 'mau') { //tìm xem có dữ liệu trong kho đối chiếu không
+        return done(null, name)
+    } else {
+        return done(null, false)
+    }
+})
+
+app.use(flash());
+app.use(session({  
+    secret: 'woot',
+    resave: false, 
+    saveUninitialized: false}));
+app.use(passport.session());
 
 app.use(express.static('public'));
 app.use(express.static('view'));
 
 app.set('view engine', 'pug')
+
+app.post('/login',
+    passport.authenticate('local', { successRedirect: '/',
+                                     failureRedirect: '/login',}));
 
 app.get('/', function (req, res) {
     console.log(req.originalUrl);
@@ -40,8 +94,8 @@ app.get('/signup', function (req, res) {
     res.sendFile(path.join(__dirname, '/views/', 'signup.html'))
 });
 
-app.get('/demo', function(req, res) {
-    res.render('demo');
-});
+// app.get('/demo', function(req, res) {
+//     res.render('demo');
+// });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}`))
