79c05583f11156e7c0a67865eddc3d07dd2fb92c
NgDMau <maunguyen.dinh@gmail.com>
2020-03-17 15:38:15 +0700

    fix certificate route

--- routes/certificate.js
+++ routes/certificate.js
@@ -2,6 +2,6 @@ var path = require('path')
 
 module.exports = function (app) {
     app.get('/certificate', function (req, res) {
-        res.sendFile(path.join(__dirname, '/views/', 'certificate.html'))
+        res.sendFile(path.join(__dirname, '../views/', 'certificate.html'))
     });
 }
