d4649258ad8adc04abe30319c02dc695a1764e88
dung <dungnguyendinh1052000@gmail.com>
2020-04-11 00:40:40 +0700

    upload breath success

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express')
 const path = require('path')
 var Pool = require('pg-pool');
 
-var flash=require("connect-flash");
+var flash = require("connect-flash");
 const session = require('express-session');
 const bodyParser = require('body-parser');
 var passport = require('passport');
@@ -11,29 +11,32 @@ var passport = require('passport');
 var cookieParser = require('cookie-parser');
 
 passport.serializeUser(function(user, done) {
-	done(null, user);
+    done(null, user);
 });
 
 passport.deserializeUser(function(user, done) {
-	done(null, user);
-});	
+    done(null, user);
+});
 
 const app = express()
 
 const port = process.env.PORT || 8000;
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 app.use(cookieParser());
 
-app.use(session({  
+app.use(session({
     key: 'user_id',
     secret: 'woot',
-    resave: false, 
+    resave: false,
     saveUninitialized: false,
     cookie: {
         expires: 60 * 1000 * 60 * 24 * 30
-    }}));
+    }
+}));
 
 app.use(express.static('public'));
 app.use(express.static('view'));
@@ -42,8 +45,8 @@ app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.set('view engine', 'ejs') 
-app.set('view engine', 'pug') 
+app.set('view engine', 'ejs')
+app.set('view engine', 'pug')
 
 
 // apply routes to the web
@@ -62,5 +65,8 @@ require('./routes/result')(app);
 require('./routes/create_project')(app);
 require('./routes/db-requester')(app);
 require('./routes/user_management')(app);
+require('./routes/upload')(app);
 
-app.listen(port, () => { console.log(`Example app listening on port ${port}` )});
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+});
--- routes/upload.js
+++ routes/upload.js
@@ -21,20 +21,29 @@ app.get('/upload', (req, res) => {
     res.sendFile(path.join(__dirname, '../views/', '/upload.html'));
 });
 app.post('/upload', upload.single('file'), (req, res) => {
-    console.log(req.file.path);
-    var key = req.file.path.split("/");
-    console.log(key[1]);
-    var data = {
-    };
-    data[key[1]] = {
-        "label": req.body.check
+    try {
+        var key = req.file.path.split("/");
+        var data = {
+        };
+        console.log(key);
+        if (req.body.check != undefined) {
+            data[key[key.length - 1]] = {
+                "label": req.body.check
+            }
+            fs.appendFile(path.join(__dirname, '../breathe/', '/data.json'), JSON.stringify(data), function(err) {
+                if (err)
+                    throw err;
+            }
+            );
+            res.sendFile(__dirname, '../views/', '/success_upload.html');
+        } else {
+            fs.unlinkSync(req.file.path);
+            res.sendFile(__dirname, '../views/', '/error_upload.html');
+
+        }
+
+    } catch ( error ) {
+        res.sendFile(__dirname, '../views/', '/error_upload.html');
     }
-    fs.appendFile(path.join(__dirname, '../breathe/', '/data.json'), JSON.stringify(data), function(err) {
-        if (err)
-            throw err;
-        console.log('complete');
-    }
-    );
-    res.redirect('/upload');
 });
 }
