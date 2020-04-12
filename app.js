63e490cdb60c02ee6ca0bdb966d9e6c4a117bb03
Thu <hathu0610@gmail.com>
2020-04-12 16:22:21 +0700

    uploadresult

--- app.js
+++ app.js
@@ -67,6 +67,7 @@ require('./routes/db-requester')(app);
 require('./routes/user_management')(app);
 require('./routes/upload')(app);
 require('./routes/uploadnew')(app);
+require('./routes/uploadresult')(app);
 require('./routes/loginrequester')(app);
 
 
