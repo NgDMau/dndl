019cf5dba714b7588b8cb56f907595eed872a0ec
vietreddevil <robin241225@gmail.com>
2020-04-12 20:52:22 +0700

    them cau hoi

---
+++ public/js/questions_training/audio.js
@@ -0,0 +1,350 @@
+questions = [
+    {
+      id: '1',
+      question: 'Chọn sắc thái thích hợp cho đoạn hội thoại dưới:',
+      src: 'audio/audio_sentiment_1.mp3',
+      answers: [
+        { text: 'Tích cực', correct: true},
+        { text: 'Tiêu cực', correct: false},
+        { text: 'Trung tính', correct: false},
+        { text: 'Không biết', correct: false}
+      ]
+    },
+    {
+      id: '2',
+      question: 'Chọn sắc thái thích hợp cho đoạn hội thoại dưới:',
+      src: 'audio/audio_sentiment_2.mp3',
+      answers: [
+        { text: 'Tích cực', correct: false},
+        { text: 'Tiêu cực', correct: true},
+        { text: 'Trung tính', correct: false},
+        { text: 'Không biết', correct: false}
+      ]
+    },
+    {
+      id: '3',
+      question: 'Chọn sắc thái thích hợp cho đoạn hội thoại dưới:',
+      src: 'audio/audio_sentiment_3.mp3',
+      answers: [
+        { text: 'Tích cực', correct: true},
+        { text: 'Tiêu cực', correct: false},
+        { text: 'Trung tính', correct: false},
+        { text: 'Không biết', correct: false}
+      ]
+    },
+    {
+      id: '4',
+      question: 'Chọn sắc thái thích hợp cho đoạn hội thoại dưới:',
+      src: 'audio/audio_sentiment_4.mp3',
+      answers: [
+        { text: 'Tích cực', correct: false},
+        { text: 'Tiêu cực', correct: false},
+        { text: 'Trung tính', correct: true},
+        { text: 'Không biết', correct: false}
+      ]
+    },
+    {
+      id: '5',
+      question: 'Chọn sắc thái thích hợp cho đoạn hội thoại dưới:',
+      src: 'audio/audio_sentiment_5.mp3',
+      answers: [
+        { text: 'Tích cực', correct: false},
+        { text: 'Tiêu cực', correct: false},
+        { text: 'Trung tính', correct: true},
+        { text: 'Không biết', correct: false}
+      ]
+    },
+    {
+      id: '6',
+      question: 'Chọn sắc thái thích hợp cho đoạn hội thoại dưới:',
+      src: 'audio/audio_sentiment_6.mp3',
+      answers: [
+        { text: 'Tích cực', correct: true},
+        { text: 'Tiêu cực', correct: false},
+        { text: 'Trung tính', correct: false},
+        { text: 'Không biết', correct: false}
+      ]
+    },
+    {
+      id: '7',
+      question: 'Chọn sắc thái thích hợp cho đoạn hội thoại dưới:',
+      src: 'audio/audio_sentiment_7.mp3',
+      answers: [
+        { text: 'Tích cực', correct: false},
+        { text: 'Tiêu cực', correct: false},
+        { text: 'Trung tính', correct: true},
+        { text: 'Không biết', correct: false}
+      ]
+    },
+    {
+      id: '8',
+      question: 'Chọn sắc thái thích hợp cho đoạn hội thoại dưới:',
+      src: 'audio/audio_sentiment_8.mp3',
+      answers: [
+        { text: 'Tích cực', correct: false},
+        { text: 'Tiêu cực', correct: false},
+        { text: 'Trung tính', correct: true},
+        { text: 'Không biết', correct: false}
+      ]
+    },
+    {
+      id: '9',
+      question: 'Chọn sắc thái thích hợp cho đoạn hội thoại dưới:',
+      src: 'audio/audio_sentiment_9.mp3',
+      answers: [
+        { text: 'Tích cực', correct: false},
+        { text: 'Tiêu cực', correct: true},
+        { text: 'Trung tính', correct: false},
+        { text: 'Không biết', correct: false}
+      ]
+    },
+    {
+      id: '10',
+      question: 'Chọn sắc thái thích hợp cho đoạn hội thoại dưới:',
+      src: 'audio/audio_sentiment_10.mp3',
+      answers: [
+        { text: 'Tích cực', correct: false},
+        { text: 'Tiêu cực', correct: false},
+        { text: 'Trung tính', correct: false},
+        { text: 'Không biết', correct: true}
+      ]
+    },
+    {
+        id: '11',
+        question: 'Chọn chủ đề thích hợp cho đoạn hội thoại dưới:',
+        src: 'audio/audio_topic_1.mp3',
+        content: 'Bây giờ có đến mấy bộ sách giáo khoa lận, không biết phải mua quyển nào cho con nó học đây',
+        answers: [
+          { text: 'Sức khỏe', correct: false},
+          { text: 'Giáo dục', correct: true},
+          { text: 'Công nghệ', correct: false},
+          { text: 'Chỗ ở', correct: false},
+          { text: 'Thuộc một chủ đề khác', correct: false}
+        ]
+      },
+      {
+        id: '12',
+        question: 'Chọn chủ đề thích hợp cho đoạn hội thoại dưới:',
+        src: 'audio/audio_topic_2.mp3',
+        content: 'Các bác cho em hỏi sao mà làm hồ sơ cho con vào trường công lập chuyên nhỉ',
+        answers: [
+          { text: 'Sức khỏe', correct: false},
+          { text: 'Giáo dục', correct: true},
+          { text: 'Công nghệ', correct: false},
+          { text: 'Chỗ ở', correct: false},
+          { text: 'Thuộc một chủ đề khác', correct: false}
+        ]
+      },
+      {
+        id: '13',
+        question: 'Chọn chủ đề thích hợp cho đoạn hội thoại dưới:',
+        src: 'audio/audio_topic_3.mp3',
+        content: 'Tôi cần mua một bộ máy tính để bàn phục vụ công việc văn phòng, giá cả dưới 10 triệu ạ',
+        answers: [
+          { text: 'Sức khỏe', correct: false},
+          { text: 'Giáo dục', correct: false},
+          { text: 'Công nghệ', correct: true},
+          { text: 'Chỗ ở', correct: false},
+          { text: 'Thuộc một chủ đề khác', correct: false}
+        ]
+      },
+      {
+        id: '14',
+        question: 'Chọn chủ đề thích hợp cho đoạn hội thoại dưới:',
+        src: 'audio/audio_topic_4.mp3',
+        content: 'năm nay, một số loại máy tính mới được ra mắt',
+        answers: [
+            { text: 'Sức khỏe', correct: false},
+            { text: 'Giáo dục', correct: false},
+            { text: 'Công nghệ', correct: true},
+            { text: 'Chỗ ở', correct: false},
+            { text: 'Thuộc một chủ đề khác', correct: false}
+          ]
+      },
+      {
+        id: '15',
+        question: 'Chọn chủ đề thích hợp cho đoạn hội thoại dưới:',
+        src: 'audio/audio_topic_5.mp3',
+        content: 'Tôi dạo này hay bị ho nóng sốt, nhưng không muốn đi khám, không biết có thuốc nam nào hiệu qủa không',
+        answers: [
+          { text: 'Sức khỏe', correct: true},
+          { text: 'Giáo dục', correct: false},
+          { text: 'Công nghệ', correct: false},
+          { text: 'Chỗ ở', correct: false},
+          { text: 'Thuộc một chủ đề khác', correct: false}
+        ]
+      },
+      {
+        id: '16',
+        question: 'Chọn chủ đề thích hợp cho đoạn hội thoại dưới:',
+        src: 'audio/audio_topic_6.mp3',
+        content: 'Mưa nắng thất thường bạn nào hoặc có người thân hay bị đau đầu,đau mỏi vai gáy nhắn tin cho em nhé, không phải uống thuốc tây nha',
+        answers: [
+            { text: 'Sức khỏe', correct: true},
+            { text: 'Giáo dục', correct: false},
+            { text: 'Công nghệ', correct: false},
+            { text: 'Chỗ ở', correct: false},
+            { text: 'Thuộc một chủ đề khác', correct: false}
+          ]
+      },
+      {
+        id: '17',
+        question: 'Chọn chủ đề thích hợp cho đoạn hội thoại dưới:',
+        src: 'audio/audio_topic_7.mp3',
+        content: 'Chỗ tôi ở khá là chật, xa trung tâm thành phố',
+        answers: [
+            { text: 'Sức khỏe', correct: false},
+            { text: 'Giáo dục', correct: false},
+            { text: 'Công nghệ', correct: false},
+            { text: 'Chỗ ở', correct: true},
+            { text: 'Thuộc một chủ đề khác', correct: false}
+          ]
+      },
+      {
+        id: '18',
+        question: 'Chọn chủ đề thích hợp cho đoạn hội thoại dưới:',
+        src: 'audio/audio_topic_8.mp3',
+        content: 'Nước đang phát triển là quốc gia có mức sống còn khiêm tốn, có nền tảng công nghiệp chưa phát triển và có chỉ số phát triển con người không cao',
+        answers: [
+            { text: 'Sức khỏe', correct: false},
+            { text: 'Giáo dục', correct: false},
+            { text: 'Công nghệ', correct: false},
+            { text: 'Chỗ ở', correct: false},
+            { text: 'Thuộc một chủ đề khác', correct: true}
+          ]
+      },
+      {
+        id: '19',
+        question: 'Chọn chủ đề thích hợp cho đoạn hội thoại dưới:',
+        src: 'audio/audio_topic_9.mp3',
+        content: 'Đầu tiên, để trở thành một doanh nhân, bạn phải liên tục học hỏi.',
+        answers: [
+            { text: 'Sức khỏe', correct: false},
+            { text: 'Giáo dục', correct: false},
+            { text: 'Công nghệ', correct: false},
+            { text: 'Chỗ ở', correct: false},
+            { text: 'Thuộc một chủ đề khác', correct: true}
+          ]
+      },
+      {
+        id: '20',
+        question: 'Chọn chủ đề thích hợp cho đoạn hội thoại dưới:',
+        src: 'audio/audio_topic_10.mp3',
+        content: 'Mình cần tìm phòng trọ quang khu vực bách khoa, điều hòa nóng lạnh đầy đủ',
+        answers: [
+            { text: 'Sức khỏe', correct: false},
+            { text: 'Giáo dục', correct: false},
+            { text: 'Công nghệ', correct: false},
+            { text: 'Chỗ ở', correct: true},
+            { text: 'Thuộc một chủ đề khác', correct: false}
+          ]
+      },
+      {
+        id: '21',
+        question: 'Chọn loại phương tiện được cho là phát ra âm thanh dưới đây:',
+        src: 'audio/audio_vehicle_1.mp3',
+        content:'tàu hỏa',
+        answers: [
+          { text: 'Máy bay', correct: false},
+          { text: 'Ô tô', correct: false},
+          { text: 'Tàu hỏa', correct: true},
+          { text: 'Một loại phương tiện khác', correct: false}
+        ]
+      },
+      {
+        id: '22',
+        question: 'Chọn loại phương tiện được cho là phát ra âm thanh dưới đây:',
+        src: 'audio/audio_vehicle_2.mp3',
+        content:'tàu hỏa',
+        answers: [
+          { text: 'Máy bay', correct: false},
+          { text: 'Ô tô', correct: false},
+          { text: 'Tàu hỏa', correct: true},
+          { text: 'Một loại phương tiện khác', correct: false}
+        ]
+      },
+      {
+        id: '23',
+        question: 'Chọn loại phương tiện được cho là phát ra âm thanh dưới đây:',
+        src: 'audio/audio_vehicle_3.mp3',
+        content:'tàu hỏa',
+        answers: [
+          { text: 'Máy bay', correct: false},
+          { text: 'Ô tô', correct: false},
+          { text: 'Tàu hỏa', correct: true},
+          { text: 'Một loại phương tiện khác', correct: false}
+        ]
+      },
+      {
+        id: '24',
+        question: 'Chọn loại phương tiện được cho là phát ra âm thanh dưới đây:',
+        src: 'audio/audio_vehicle_4.mp3',
+        content:'ô tô',
+        answers: [
+          { text: 'Máy bay', correct: false},
+          { text: 'Ô tô', correct: true},
+          { text: 'Tàu hỏa', correct: false},
+          { text: 'Một loại phương tiện khác', correct: false}
+        ]
+      },
+      {
+        id: '25',
+        question: 'Chọn loại phương tiện được cho là phát ra âm thanh dưới đây:',
+        src: 'audio/audio_vehicle_5.mp3',
+        content:'ô tô',
+        answers: [
+          { text: 'Máy bay', correct: false},
+          { text: 'Ô tô', correct: true},
+          { text: 'Tàu hỏa', correct: false},
+          { text: 'Một loại phương tiện khác', correct: false}
+        ]
+      },
+      {
+        id: '26',
+        question: 'Chọn loại phương tiện được cho là phát ra âm thanh dưới đây:',
+        src: 'audio/audio_vehicle_6.mp3',
+        content:'ô tô',
+        answers: [
+          { text: 'Máy bay', correct: false},
+          { text: 'Ô tô', correct: true},
+          { text: 'Tàu hỏa', correct: false},
+          { text: 'Một loại phương tiện khác', correct: false}
+        ]
+      },
+      {
+        id: '27',
+        question: 'Chọn loại phương tiện được cho là phát ra âm thanh dưới đây:',
+        src: 'audio/audio_vehicle_7.mp3',
+        content:'máy bay',
+        answers: [
+          { text: 'Máy bay', correct: true},
+          { text: 'Ô tô', correct: false},
+          { text: 'Tàu hỏa', correct: false},
+          { text: 'Một loại phương tiện khác', correct: false}
+        ]
+      },
+      {
+        id: '28',
+        question: 'Chọn loại phương tiện được cho là phát ra âm thanh dưới đây:',
+        src: 'audio/audio_vehicle_8.mp3',
+        content:'máy bay',
+        answers: [
+          { text: 'Máy bay', correct: true},
+          { text: 'Ô tô', correct: false},
+          { text: 'Tàu hỏa', correct: false},
+          { text: 'Một loại phương tiện khác', correct: false}
+        ]
+      },
+      {
+        id: '29',
+        question: 'Chọn loại phương tiện được cho là phát ra âm thanh dưới đây:',
+        src: 'audio/audio_vehicle_9.mp3',
+        content:'máy bay',
+        answers: [
+          { text: 'Máy bay', correct: true},
+          { text: 'Ô tô', correct: false},
+          { text: 'Tàu hỏa', correct: false},
+          { text: 'Một loại phương tiện khác', correct: false}
+        ]
+      }  
+  ]
