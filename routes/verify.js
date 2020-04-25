a2446f8e579379f87ba6fa0995789b26bf09c8aa
dung <dungnguyendinh1052000@gmail.com>
2020-04-25 21:20:36 +0700

    verify user

---
+++ routes/verify.js
@@ -0,0 +1,52 @@
+var Pool = require('pg-pool');
+var CryptoJS = require("crypto-js");
+
+const pool = new Pool({
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_DATABASE,
+    ssl: true //(process.env.DB_SSL == 'true')
+});
+
+function decode(encrypted) {
+    var username = CryptoJS.AES.decrypt(encrypted, "inlabinfore");
+    return username.toString(CryptoJS.enc.Utf8);
+}
+module.exports = function(app) {
+app.get('/verify', (req, res) => {
+    if (req.query.hash) {
+        try {
+            var hash = req.query.hash;
+            console.log(hash);
+            var username = decode(hash).split("_date_")[0];
+            verifyUser(username);
+            async function verifyUser(hash) {
+                const client = await pool.connect();
+                try {
+                    await client.query('BEGIN');
+                    await client.query("UPDATE users SET verify=1 WHERE username=($1)", [username], function(err, result) {
+                        if (err) {
+                            client.release();
+                        } else {
+                            res.send("Success verify");
+                            client.release();
+                        }
+                    });
+
+                } catch ( e ) {
+                    throw (e);
+                }
+            }
+
+        } catch ( e ) {
+            throw (e);
+        }
+
+    } else {
+        res.send("error");
+    }
+
+})
+}
