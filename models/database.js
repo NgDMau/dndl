05fec5a573073a393781b7e54dcb5010d8ff9acd
DuongPhan45 <47710293+DuongPhan45@users.noreply.github.com>
2020-05-11 02:23:33 -0400

    Dashboard Error

--- models/database.js
+++ models/database.js
@@ -1,11 +1,16 @@
 var Pool = require('pg-pool');
 
 const connection = new Pool({
-    user: process.env.user || 'mpndhiboquobry',
-    password: process.env.password || '92cf533ac275e9f7a116c6eb8e79477b3fa074679712bf8165a08f834db679f5',
-    host: process.env.host || 'ec2-3-229-210-93.compute-1.amazonaws.com',
+    // user: process.env.user || 'mpndhiboquobry',
+    user: process.env.user || 'qrmmeltkivmkot',
+    password: process.env.password || 'a3582d2e31948a9bdff3cd8e76c3824358a09d8fa2f9cb4d931bb5247f691f7e',
+    host: process.env.host || 'ec2-18-210-214-86.compute-1.amazonaws.com',
     port: process.env.port || '5432',
-    database: process.env.database || 'd5tabqes3975',
+    database: process.env.database || 'd1r7gpirn3t2e6',
+    // password: process.env.password || '92cf533ac275e9f7a116c6eb8e79477b3fa074679712bf8165a08f834db679f5',
+    // host: process.env.host || 'ec2-3-229-210-93.compute-1.amazonaws.com',
+    // port: process.env.port || '5432',
+    // database: process.env.database || 'd5tabqes3975',
     ssl: true
 });
 
