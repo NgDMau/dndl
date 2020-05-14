c725d9c47d91ed6a6043de048ecc6aad9bb7edcc
dung <dungnguyendinh1052000@gmail.com>
2020-05-14 19:17:02 +0700

    change password

--- models/send_mail.js
+++ models/send_mail.js
@@ -1,35 +1,45 @@
 const nodemailer = require('nodemailer');
 var CryptoJS = require("crypto-js");
 
+email_from = process.env.EMAIL_USERNAME || "inlabvietnam";
+pass_email = process.env.EMAIL_PASSWORD || "Nisci123";
+
+
+
+module.exports = {send_mail,decode,encode};
+
+function decode(encrypted) {
+    var username = CryptoJS.AES.decrypt(encrypted, "inlabinfore");
+    return username.toString(CryptoJS.enc.Utf8);
+}
+
 function encode(username) {
     var hash = CryptoJS.AES.encrypt(username + "_date_" + Date.now(), "inlabinfore");
     return encodeURIComponent(hash.toString());
 }
 
-module.exports = function(user, pass, email_to, username) {
+function send_mail(email_to, username, subject, html) {
 
-var transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-        user: user,
-        pass: pass
-    }
-});
+    var transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: email_from,
+            pass: pass_email
+        }
+    });
 
-var link = "http://inlab.nisci.gov.vn/verify?hash=" + encode(username);
+    var mailOptions = {
+        from: email_from,
+        to: email_to,
+        subject: subject,
+        html: html
+    };
 
-var mailOptions = {
-    from: user,
-    to: email_to,
-    subject: 'Inlab verify signup',
-    html: "Hello,<br> Please Click on the link to verify your email.<br><a href=" + link + ">Click here to verify</a>"
-};
-
-transporter.sendMail(mailOptions, function(error, info) {
-    if (error) {
-        console.log(error);
-    } else {
-        console.log('Email sent: ' + info.response);
-    }
-});
+    transporter.sendMail(mailOptions, function(error, info) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Email sent: ' + info.response);
+        }
+    })
 }
