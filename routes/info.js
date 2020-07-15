2c81cc481a84d00c54c88c609e38f607fc103317
ltta18 <thuylinh7720@gmail.com>
2020-07-15 19:58:41 +0700

    Add Project Manage Screen

--- routes/info.js
+++ routes/info.js
@@ -3,6 +3,6 @@ var path = require('path')
 module.exports = function (app) {
     app.get('/info', function (req, res) {
         // res.sendFile(path.join(__dirname, '../views/', 'info.html'))
-        res.render("info.ejs")
+        res.render("info2.ejs")
     });
 }
