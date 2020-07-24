fcf45e371e2ad3a08db13a2b884d6de432613d29
u04 <u04@localhost.localdomain>
2020-07-24 10:47:12 +0700

    add things

---
+++ routes/demo.js
@@ -0,0 +1,6 @@
+module.exports = function (app) {
+    app.get('/demo/image_polygons', function (req, res) {
+        // res.sendFile(path.join(__dirname, '../views/', 'info.html'))
+        res.render("full_system/image_polygons.ejs");
+    });
+}
