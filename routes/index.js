7b91786ab4e09f84578922025de2e3cee94da77f
Mau D. Ng <maunguyen.dinh@gmail.com>
2020-07-27 14:54:28 +0700

    add logging line

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var path = require('path')
 module.exports = function (app) {
     app.get('/', function (req, res) {
         //res.sendFile(path.join(__dirname, '../views/', 'about.ejs'))
+        console.log(req)
         res.render('about.ejs')
     });
 }
