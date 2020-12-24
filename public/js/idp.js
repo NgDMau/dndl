c226b139eedd78cdb5a14523f357a39369cbb011
NgDMau <maunguyen.dinh@gmail.com>
2020-12-25 01:44:18 +0700

    fixed bug not-loading-new-img

--- public/js/idp.js
+++ public/js/idp.js
@@ -32,6 +32,32 @@ const convertTask = function(task) {
       };
 };
 
+const _convertTask = function(task) {
+    // converts the task from the server format to the format
+    // supported by the LS frontend
+    if (!task) return;
+  
+    if (task.completions) {
+      for (let tc of task.completions) {
+        tc.pk = tc.id;
+        tc.createdAgo = tc.created_ago;
+        tc.createdBy = tc.created_username;
+        tc.leadTime = tc.lead_time;
+      }
+    }
+  
+    if (task.predictions) {
+      for (let tp of task.predictions) {
+        tp.pk = tp.pk;
+        tp.createdAgo = tp.created_ago;
+        tp.createdBy = tp.created_by;
+        tp.createdDate = tp.created_date;
+      }
+    }
+  
+    return task;
+  };
+
 async function initTask(project_id) {
     return await this.getNextTask(project_id);
 }
